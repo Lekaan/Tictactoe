e976b6c7cbd6f16c3c560bea7116f51ef0497ea3
kaans <kaans@DESKTOP-U4CBDT5>
2016-10-06 08:44:33 +0200

    Add project files.

---
+++ TicTacToe.UnitTests/GameBoard.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using TicTacToe.Services;
+
+namespace TicTacToe.UnitTests
+{
+    internal class GameBoard
+    {
+        private static bool currentPlayer = true;
+        private static int coord1;
+        private static int coord2;
+        private static char[,] currentGameBoard = new char[3, 3]
+        {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+        
+
+        internal static char[,] GetGameBoard()
+        {
+            return currentGameBoard;
+        }
+
+        
+
+        internal static bool GetPlayer()
+        {
+            return currentPlayer;
+        }
+
+        internal static void SetCurrentPlayer()
+        {
+            if (currentPlayer)
+                currentPlayer = false;
+            else
+                currentPlayer = true;
+        }
+
+        internal static bool CanConvertPlayerInput(string input)
+        {
+            int temp1 = Int32.Parse(input.Substring(0, 1));
+            int temp2 = Int32.Parse(input.Substring(2, 1));
+
+            if (temp1 >= 0 && temp1 <= 2 && temp2 >= 0 && temp2 <= 2)
+            {
+                coord1 = temp1;
+                coord2 = temp2;
+                
+                return true;
+            }
+            
+            return false;
+        }
+
+        internal static char GetGameBoardField()
+        {
+            return currentGameBoard[coord1,coord2]; 
+        }
+
+        public static char[,] GetSelectedField()
+        {
+            return new char[coord1,coord2];
+        }
+
+        internal static int CountPlayerIcons()
+        {
+            int iconCount = 0;
+            char playerIcon = GetPlayerIcon();
+            foreach (char c in currentGameBoard)
+            {
+                if (c.Equals(playerIcon))
+                    iconCount++;
+            }
+            return iconCount;
+        }
+
+        public static char GetPlayerIcon()
+        {
+            char PlayerIcon = 'X';
+            if (!currentPlayer)
+                PlayerIcon = 'O';
+
+            return PlayerIcon;
+        }
+
+        internal static void PopulateField()
+        {
+            
+
+            currentGameBoard[coord1, coord2] = GetPlayerIcon();
+        }
+    }
+}
---
+++ TicTacToe.UnitTests/GameBoardTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SqlServer.Server;
+using NUnit.Framework;
+using TicTacToe.Services;
+
+namespace TicTacToe.UnitTests
+{
+    [TestFixture]
+    class GameBoardTest
+    {
+        private char[,] _gameBoard;
+
+        [SetUp]
+        public void SetupUnitTests()
+        {
+            _gameBoard = new char[3, 3]
+                  {
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '},
+                      {' ', ' ', ' '}
+                  };
+        }
+        [Test]
+        public void CanGetGameBoard()
+        {
+            Assert.AreEqual(_gameBoard, GameBoard.GetGameBoard());
+        }
+
+        [Test]
+        public void CanCheckPlayerInput()
+        {
+            Assert.IsTrue(GameBoard.CanConvertPlayerInput("0.2"));
+            Assert.IsFalse(GameBoard.CanConvertPlayerInput("4.5"));
+
+        }
+
+        [Test]
+        public void CanGetPlayer()
+        {
+            Assert.IsTrue(GameBoard.GetPlayer());
+            GameBoard.SetCurrentPlayer();
+            Assert.IsFalse(GameBoard.GetPlayer());
+            GameBoard.SetCurrentPlayer();
+
+        }
+        [Test]
+        public void CanSetGameBoard()
+        {
+            Assert.AreEqual(' ', GameBoard.GetGameBoardField());
+            Assert.AreEqual(0, GameBoard.CountPlayerIcons());
+            Assert.LessOrEqual(GameBoard.CountPlayerIcons(), 3);
+
+            GameBoard.PopulateField();
+            Assert.AreEqual('X', GameBoard.GetGameBoardField());
+
+            Assert.AreEqual(1, GameBoard.CountPlayerIcons());
+
+            GameWinnerService gm = new GameWinnerService();
+            Assert.AreEqual(' ', gm.Validate(GameBoard.GetGameBoard()));
+        }
+
+        //public void CanPrintBoard()
+        //{
+
+        //    string board =
+        //        "       #       #                        #       #       \n" +
+        //        "       #       #                  (0,0) # (0,1) # (0,2) \n" +
+        //        "       #       #                        #       #       \n" +
+        //        "#######################          #######################\n" +
+        //        "       #       #                        #       #       \n" +
+        //        "       #       #                  (1,0) # (1,1) # (1,2) \n" +
+        //        "       #       #                        #       #       \n" +
+        //        "#######################          #######################\n" +
+        //        "       #       #                        #       #       \n" +
+        //        "       #       #                  (2,0) # (2,1) # (2,2) \n" +
+        //        "       #       #                        #       #       \n";
+
+        //    GameBoard.
+
+
+        //    Assert.AreEqual(board, GameBoard.GetBoard());
+        //}
+    }
+}
